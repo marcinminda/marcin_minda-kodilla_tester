04af3ac619377cba92c6c3a4857acd33b6fb9ff2
Marcin Minda <marcin.minda@gmail.com>
2024-12-11 17:46:03 +0100

    klasa hello world

---
+++ kodilla-intro/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Marcin");
+        System.out.println(39);
+        System.out.println('M');
+    }
+}
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Marcin");
+        System.out.println(39);
+        System.out.println('M');
+    }
+
+}
